a975e39990fa3bf36cce4bad1b74af8437caa028
Artsiom <artsiom.andryianau@gmail.com>
2019-08-25 01:57:20 +0200

    Commit2

--- src/main/java/com/example/treeter/repos/MessageRepo.java
+++ src/main/java/com/example/treeter/repos/MessageRepo.java
@@ -1,7 +1,12 @@
 package com.example.treeter.repos;
 
-import com.example.treeter.domain.Message
+import com.example.treeter.domain.Message;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface MessageRepo extends CrudRepository<Message, Long> {
+
+    List<Message> findByTag(String tag);
+
 }
